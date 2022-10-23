8f6d5eeb8ec8ae62c32c57b23cf0676d589ab76a
David Mtz <davidelimg1@gmail.com>
2022-10-22 20:31:29 -0500

    Added constants.

--- lib/application.rb
+++ lib/application.rb
@@ -4,6 +4,9 @@ require_relative './interactors/serial_interactor'
 require_relative './services/scroller_service'
 
 class Application
+  REQUEST_ANIMATION = 'req-an-01'.freeze
+  CONFIRM_REQUEST = 'con-an-01'.freeze
+  FINISHED_ANIMATION = 'cust-ends'.freeze
   @setup = nil
 
   class << self
@@ -33,20 +36,21 @@ class Application
       DisplayInteractor.print_menu
       selection = gets.chomp
       puts 'hello'
-      @setup.perform && DisplayInteractor.print_perfom if selection.eql?('1')
+      @setup.perform && DisplayInteractor.print_listening if selection.eql?('1')
       sleep 1
     end
 
     def performing
       sleep 0.5
-      SerialInteractor.send_data(@setup, 'req-an-01')
+      SerialInteractor.send_data(@setup, REQUEST_ANIMATION)
       response = SerialInteractor.read_data(@setup)
-      SerialInteractor.send_data(@setup, 'con-an-01') if response.eql?('req-an-01')
+      SerialInteractor.send_data(@setup, CONFIRM_REQUEST) if response.eql?(REQUEST_ANIMATION)
       custom_text = gets.chomp
+      DisplayInteractor.print_listening
       sleep 0.5
       SerialInteractor.send_data(@setup, custom_text)
       response = SerialInteractor.read_data(@setup)
-      @setup.reset unless response.eql?('cust-ends')
+      @setup.reset unless response.eql?(FINISHED_ANIMATION)
       sleep 1
     end
   end
